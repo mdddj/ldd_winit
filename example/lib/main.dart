c19e071adbef250b5aab0eb6450f3a447fbe1e31
梁典典 <413153189@qq.com>
2024-07-12 17:29:50 +0800

    0.0.1

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:ldd_winit/ldd_winit.dart' as ldd_winit;
+import 'package:ldd_winit/api/winit.dart';
+import 'package:ldd_winit/frb_generated.dart';
 
 void main() {
+  RustLib.init();
   runApp(const MyApp());
 }
 
@@ -15,57 +15,28 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late int sumResult;
-  late Future<int> sumAsyncResult;
-
   @override
   void initState() {
     super.initState();
-    sumResult = ldd_winit.sum(1, 2);
-    sumAsyncResult = ldd_winit.sumAsync(3, 4);
+  }
+
+  Future<void> getDisplayInfoList() async {
+    List<LddDisplayInfo> displatInfoList = await lddGetWindowsMonitors();
   }
 
   @override
   Widget build(BuildContext context) {
-    const textStyle = TextStyle(fontSize: 25);
-    const spacerSmall = SizedBox(height: 10);
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Native Packages'),
         ),
         body: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-                const Text(
-                  'This calls a native function through FFI that is shipped as source in the package. '
-                  'The native code is built as part of the Flutter Runner build.',
-                  style: textStyle,
-                  textAlign: TextAlign.center,
-                ),
-                spacerSmall,
-                Text(
-                  'sum(1, 2) = $sumResult',
-                  style: textStyle,
-                  textAlign: TextAlign.center,
-                ),
-                spacerSmall,
-                FutureBuilder<int>(
-                  future: sumAsyncResult,
-                  builder: (BuildContext context, AsyncSnapshot<int> value) {
-                    final displayValue =
-                        (value.hasData) ? value.data : 'loading';
-                    return Text(
-                      'await sumAsync(3, 4) = $displayValue',
-                      style: textStyle,
-                      textAlign: TextAlign.center,
-                    );
-                  },
-                ),
-              ],
-            ),
+          child: Column(
+            children: [
+              TextButton(
+                  onPressed: getDisplayInfoList, child: const Text("获取所有屏幕信息"))
+            ],
           ),
         ),
       ),
---
+++ rust/src/api/winit.rs
@@ -0,0 +1,52 @@
+use display_info::DisplayInfo;
+
+///读取已连接屏幕信息
+pub fn ldd_get_windows_monitors() -> Vec<LddDisplayInfo> {
+    let display_infos = DisplayInfo::all().unwrap();
+    display_infos.into_iter().map(|v| v.into()).collect()
+}
+
+///屏幕信息
+#[derive(Debug, Clone)]
+pub struct LddDisplayInfo {
+    /// 显示名称
+    pub name: String,
+    /// 与显示器关联的唯一标识符。
+    pub id: u32,
+    /// 本机屏幕原始句柄
+    pub raw_handle: u32,
+    /// 显示器x坐标
+    pub x: i32,
+    /// 显示器y坐标。
+    pub y: i32,
+    /// 显示像素宽度。
+    pub width: u32,
+    /// 显示像素高度。
+    pub height: u32,
+    /// 可以是0、90、180、270，表示屏幕按时钟方向旋转的度数。
+    pub rotation: f32,
+    /// 输出设备的像素比例因子。
+    pub scale_factor: f32,
+    /// 显示刷新率。
+    pub frequency: f32,
+    /// 屏幕是否为主屏幕
+    pub is_primary: bool,
+}
+
+impl Into<LddDisplayInfo> for DisplayInfo {
+    fn into(self) -> LddDisplayInfo {
+        LddDisplayInfo {
+            name: self.name,
+            id: self.id,
+            raw_handle: self.raw_handle.id,
+            x: self.x,
+            y: self.y,
+            width: self.width,
+            height: self.height,
+            rotation: self.rotation,
+            scale_factor: self.scale_factor,
+            frequency: self.frequency,
+            is_primary: self.is_primary,
+        }
+    }
+}
